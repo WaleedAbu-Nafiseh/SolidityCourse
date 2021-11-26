b72781283fc6ae4368a10c3488be312d9dde1ae1
Waleed Abu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2021-11-26 15:34:34 +0200

    Add deploy method

--- contract-01/deploy.js
+++ contract-01/deploy.js
@@ -1,1 +1,20 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+//ADD the 12 words Mnemonic below
+var MNEMIONIC = ""
+var INFURA_DEPLOY_LINK = "";
+
+const provider = new HDWalletProvider(MNEMIONIC, INFURA_DEPLOY_LINK);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Deploying from accont : ' + accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There mate'] })
+        .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract is available on : ' + result.options.address)
+};
+deploy();
