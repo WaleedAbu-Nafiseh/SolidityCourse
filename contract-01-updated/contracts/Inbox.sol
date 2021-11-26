c8be01e2f80df84eb6d48447113de54218a73ead
Waleed Abu-Nafiseh <43027952+WaleedAbu-Nafiseh@users.noreply.github.com>
2021-11-26 17:10:00 +0200

    Update project

---
+++ contract-01-updated/contracts/Inbox.sol
@@ -0,0 +1,17 @@
+// contract code will go here
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+ 
+contract Inbox {
+    string public message;
+    
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
+
